e6bd1db2117473534f583cbfa0d156d21db93efe
Sergio <norgestream@gmail.com>
2024-04-26 22:10:53 +0300

    create ability to add new products to cart

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,34 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const data = [{
+    id: 'p1',
+    price: 11,
+    title: 'Toilet paper',
+    description: 'The softest paper you ever feel'
+  },
+  {
+    id: 'p2',
+    price: 8,
+    title: 'Soap',
+    description: 'No need desription'
+  }
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {data.map(product => 
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        )}
       </ul>
     </section>
   );
